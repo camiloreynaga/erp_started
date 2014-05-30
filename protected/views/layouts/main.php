a167e5526cec234bd62cb1056c177bfe0c5f3f08
camilo reynaga <camilo.reynaga@gmail.com>
2014-05-30 18:45:36 -0500

    Menu de mantenimiento Producto

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -26,15 +26,27 @@
 		<div id="logo"><?php echo CHtml::encode(Yii::app()->name); ?></div>
 	</div><!-- header -->
 
-	<div id="mainmenu">
-		<?php $this->widget('zii.widgets.CMenu',array(
+	<div id="mainMbmenu">
+		<?php $this->widget('application.extensions.mbmenu.MbMenu',array(
 			'items'=>array(
 				array('label'=>'Home', 'url'=>array('/site/index')),
 				array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
 				array('label'=>'Contact', 'url'=>array('/site/contact')),
+                                array('label'=>'Mantenimiento',
+                                    'items'=>array(
+                                        array('label'=>'presentacion','url'=>array('/presentacion')),
+                                        array('label'=>'tipo producto','url'=>array('/tipoProducto')),
+                                        array('label'=>'fabricante','url'=>array('/fabricante')),
+                                        array('label'=>'caracteristica','url'=>array('/caracteristica')),
+                                        array('label'=>'producto','url'=>array('/producto')),
+                                    
+                                
+                                        ),
+                                    ),
 				array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
 				array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest)
 			),
+                        
 		)); ?>
 	</div><!-- mainmenu -->
 	<?php if(isset($this->breadcrumbs)):?>
