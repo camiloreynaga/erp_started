bf270b3e4f831d23a71ee8e10bd442e8d44b3e83
Fardisur03 <Fardisur03@192.168.1.34>
2014-09-11 18:48:14 -0500

    migracion php proveedor ok

--- facturacion2/migracion_01.php
+++ facturacion2/migracion_01.php
@@ -23,8 +23,36 @@ $data = true;
         
     } 
 
- if(consulta("INSERT INTO tbl_proveedor(id,nombre_rz, ruc, contacto, direccion, ciudad, telefono, linea_credito, credito_disponible,activo,create_time,create_user_id,update_time,update_user_id)"
-            . "SELECT id, nombre_proveedor, ruc,contacto,direccion,ciudad,telefono,0,0,fecha,1,fecha,1), "
-            . " FROM producto;")){
+ if(consulta("INSERT INTO erp_started2.tbl_proveedor"
+         . "(id," //1
+         . "nombre_rz," //2
+         . " ruc," //3
+         . " contacto," //4 
+         . " direccion," //5
+         . " ciudad," //6
+         . " telefono, " //7
+         . "linea_credito, " //8
+         . "credito_disponible," //9
+         . "activo," //10
+         . "create_time," //11
+         . "create_user_id," //12
+         . "update_time," //13
+         . "update_user_id)" //14
+            . "SELECT " 
+         . "id," //1
+         . "nombre_proveedor," //2
+         . " ruc," //3
+         . "contacto," //4
+         . "direccion," //5
+         . "ciudad," //6
+         . "telefono," //7
+         . "0," //8
+         . "0," //9
+         . "0,"
+         . "fecha," //10
+         . "1," //11
+         . "fecha," //12
+         . "1 " //13
+            . " FROM drogueria2.proveedor;")){
             printf("PROVEEDORES MIGRADOS CON EXITO\n");
     }
