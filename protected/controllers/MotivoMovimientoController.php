37f95bb199c95eb939358c8065152aa79a6ed6e6
camilo reynaga <camilo.reynaga@gmail.com>
2014-10-14 18:48:12 -0500

    acceso a motivo movimiento

--- protected/controllers/MotivoMovimientoController.php
+++ protected/controllers/MotivoMovimientoController.php
@@ -36,7 +36,7 @@ class MotivoMovimientoController extends Controller
         ),
         array('allow', // allow admin user to perform 'admin' and 'delete' actions
         'actions'=>array('admin','delete'),
-        'users'=>array('admin,deysi'),
+        'users'=>array('admin','deysi'),
         ),
         array('deny',  // deny all users
         'users'=>array('*'),
